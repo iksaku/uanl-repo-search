a18c8c99edae84802a9ead6b466de1e909a89901
Jorge González <iksaku@me.com>
2019-01-10 16:38:03 -0600

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div id="topics">
+            <label v-for="(topic, index) in topics" :key="index">
+                <input type="checkbox" :id="topic" :value="topic" v-model="checkedTopics">
+                {{ topic }}
+            </label>
+        </div>
+
+        <div v-if="repos.length > 0">
+            <p v-for="(repo, index) in repos" :key="index">
+                <span>{{ repo.full_name }}</span>
+            </p>
+        </div>
+        <div v-else>
+            <span>No repositories loaded</span>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                repos: [],
+                topics: ['uanl', 'fime', 'its'],
+                checkedTopics: ['uanl', 'fime'],
+                token: null
+            }
+        },
+        watch: {
+            checkedTopics() {
+                this.fetchData()
+            }
+        },
+        methods: {
+            fetchData() {
+
+                if (this.checkedTopics.length < 1) {
+                    this.repos = []
+                    return
+                }
+
+                let query = this.checkedTopics.map(topic => 'topic:' + topic).join('+')
+
+                axios.get('https://api.github.com/search/repositories?q=' + query)
+                    .then((response) => {
+                        console.log(response.data)
+                        this.repos = response.data.items
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            }
+        },
+        beforeMount() {
+            this.fetchData()
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
