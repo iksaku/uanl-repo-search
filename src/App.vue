ced8ae190646f8f13524b4e9fe3ab4d00eba0be0
Jorge González <iksaku@me.com>
2019-01-12 18:10:33 -0600

    Vuetify things!

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,126 @@
 <template>
-    <div id="app">
-        <div id="topics">
-            <label v-for="(topic, index) in topics" :key="index">
-                <input type="checkbox" :id="topic" :value="topic" v-model="checkedTopics">
-                {{ topic }}
-            </label>
-        </div>
+    <v-app>
+        <v-toolbar app dark clipped-left>
+            <v-toolbar-side-icon
+                    class="hidden-lg-and-up"
+                    @click.stop="drawer = !drawer"
+            ></v-toolbar-side-icon>
 
-        <div v-if="repos.length > 0">
-            <p v-for="(repo, index) in repos" :key="index">
-                <span>{{ repo.full_name }}</span>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>UANL Repo Search</span>
+            </v-toolbar-title>
+        </v-toolbar>
+
+        <v-navigation-drawer app clipped dark v-model="drawer">
+            <v-toolbar flat dense>
+                <v-btn block flat href="https://github.com/iksaku/uanl-repo-search" target="_blank">
+                    <span class="mr-2">Contribute on Github</span>
+                    <v-icon>fab fa-github</v-icon>
+                </v-btn>
+            </v-toolbar>
+
+            <v-toolbar flat dense>
+                <v-tooltip bottom color="#0a0a0a" v-model="rateTooltip">
+                    <span slot="activator"></span>
+                    <span>
+                            This is the available requests' count your computer have to ping Github's servers in order
+                            to search for the selected topics.
+                            <br>
+                            This count resets every minute after first request.
+                            <br>
+                            To learn more about it please refer to
+                            <a href="https://developer.github.com/v3/search/#rate-limit" target="_blank">
+                                Github's Documentation
+                            </a>
+                        </span>
+                </v-tooltip>
+
+                <span class="justify-self-center">
+                    <!-- TODO: Align this to the center -->
+
+                    Available Requests: {{ rate.remaining }} / {{ rate.limit }}
+
+                    <v-icon right small @click.stop="rateTooltip = !rateTooltip">fas fa-question-circle</v-icon>
+                </span>
+            </v-toolbar>
+
+            <v-list>
+                <v-list-tile v-for="(topic, index) in topics" :key="index">
+                    <v-list-tile-title>
+                        <label>
+                            <input type="checkbox" :id="topic" :value="topic" v-model="selectedTopics">
+                            {{ topic.toUpperCase() }}
+                        </label>
+                    </v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-content>
+            <p v-for="repo in repos" :key="repo['id']">
+                <a :href="repo['html_url']" target="_blank">
+                    {{ repo['full_name'] }}
+                </a>
             </p>
-        </div>
-        <div v-else>
-            <span>No repositories loaded</span>
-        </div>
-    </div>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
     import axios from 'axios'
 
     export default {
-        name: 'app',
+        name: 'App',
         data() {
             return {
+                drawer: null,
+                rateTooltip: null,
+
+                selectedTopics: ['uanl', 'fime'],
+                topics: [
+                    'uanl',
+                    'fime',
+                    'its'
+                ],
                 repos: [],
-                topics: ['uanl', 'fime', 'its'],
-                checkedTopics: ['uanl', 'fime'],
+                rate: {
+                    limit: 0,
+                    remaining: 0,
+                    reset: null
+                },
                 token: null
             }
         },
         watch: {
-            checkedTopics() {
+            selectedTopics() {
                 this.fetchData()
             }
         },
         methods: {
             fetchData() {
-
-                if (this.checkedTopics.length < 1) {
-                    this.repos = []
+                if (this.selectedTopics.length < 1) {
+                    console.log('Fetch skipped')
                     return
                 }
 
-                let query = this.checkedTopics.map(topic => 'topic:' + topic).join('+')
+                let query = this.selectedTopics.map(topic => 'topic:' + topic).join('+')
 
                 axios.get('https://api.github.com/search/repositories?q=' + query)
                     .then((response) => {
-                        console.log(response.data)
+                        console.log(response.data.items)
                         this.repos = response.data.items
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+                    .then(() => {
+                        this.remainingLimit()
+                    })
+            },
+            remainingLimit() {
+                axios.get('https://api.github.com/rate_limit')
+                    .then((response) => {
+                        this.rate = response.data['resources']['search']
                     })
                     .catch((error) => {
                         console.log(error)
@@ -61,14 +132,3 @@
         }
     }
 </script>
-
-<style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
