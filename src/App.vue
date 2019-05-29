dd9dfda96e4452141589649d7f10fb8ba8519da3
Jorge González <iksaku@me.com>
2019-05-29 17:51:30 -0500

    Trying TravisCI's multi stage build

--- src/App.vue
+++ src/App.vue
@@ -114,7 +114,7 @@
         watch: {
             selectedTopics() {
                 this.fetchData()
-            },
+            }
         },
         computed: {
             rateLimitExceeded() {
