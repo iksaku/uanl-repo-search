febe4bbbdb522be1fc1782eae01096facc1cd044
Jorge González <iksaku@me.com>
2020-12-17 01:50:24 +0000

    Fix Linting

--- components/Logo.vue
+++ components/Logo.vue
@@ -22,14 +22,14 @@
 </template>
 
 <style>
-.NuxtLogo {
-  animation: 1s appear;
-  margin: auto;
-}
-
-@keyframes appear {
-  0% {
-    opacity: 0;
+  .NuxtLogo {
+    animation: 1s appear;
+    margin: auto;
   }
-}
+
+  @keyframes appear {
+    0% {
+      opacity: 0;
+    }
+  }
 </style>
