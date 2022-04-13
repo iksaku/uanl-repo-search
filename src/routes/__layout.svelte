e145b38442a681c7cd2c043aca91b6422532e2d3
Jorge González <yo@jorgeglz.io>
2022-04-13 00:02:40 -0500

    Use OpenGraph meta names

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,11 +7,13 @@
     <link rel='icon' href={logo} type="image/svg+xml" />
 
     <title>UANL Repo Search</title>
-    <meta name="description" content="Descubre proyectos creados por alumnos y docentes de la Universidad Autónoma de Nuevo León">
+    <meta name="og:title" content="UANL Repo Search">
+    <meta name="og:description" content="Descubre proyectos creados por alumnos y docentes de la Universidad Autónoma de Nuevo León">
     <meta name="og:image" content={logo}>
 
     <meta name="twitter:card" content="summary">
     <meta name="twitter:site" content="@iksaku2">
+    <meta name="twitter:creator" content="@iksaku2">
 </svelte:head>
 
 <slot></slot>
