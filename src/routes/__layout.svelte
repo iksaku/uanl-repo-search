540cf155741796c55862129eb3414480b6464acf
Jorge González <yo@jorgeglz.io>
2022-04-12 23:59:32 -0500

    Add twitter card

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,6 +9,9 @@
     <title>UANL Repo Search</title>
     <meta name="description" content="Descubre proyectos creados por alumnos y docentes de la Universidad Autónoma de Nuevo León">
     <meta name="og:image" content={logo}>
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@iksaku2">
 </svelte:head>
 
 <slot></slot>
