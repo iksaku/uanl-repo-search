7d09b52777fc21ba8af77a74d6b20870df1793f2
Jorge González <yo@jorgeglz.io>
2022-04-12 23:55:46 -0500

    Forgot page title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,8 @@
 
 <svelte:head>
     <link rel='icon' href={logo} type="image/svg+xml" />
+
+    <title>UANL Repo Search</title>
     <meta name="description" content="Descubre proyectos creados por alumnos y docentes de la Universidad Autónoma de Nuevo León">
     <meta name="og:image" content={logo}>
 </svelte:head>
