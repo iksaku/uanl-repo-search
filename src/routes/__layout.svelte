ed6599fe91d2ee9066c54f82dde531a3588c22aa
Jorge González <yo@jorgeglz.io>
2022-06-28 23:42:25 -0500

    Transpile logo to PNG, update dependencies

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,13 @@
 <script>
     import '$assets/app.css'
-    import logo from '$assets/logo.svg?url'
+
+    import svg from '$assets/logo.svg'
+    import png from '$assets/logo.svg?width=32&height=32&format=png'
 </script>
 
 <svelte:head>
-    <link rel='icon' href={logo} type="image/svg+xml" />
+    <link rel="icon" type="image/svg+xml" href={svg} />
+    <link rel="alternate icon" type="image/png" sizes="any" href={png} />
 
     <title>UANL Repo Search</title>
     <meta name="description" content="Descubre proyectos creados por alumnos y docentes de la Universidad Autónoma de Nuevo León">
@@ -12,7 +15,7 @@
     <meta name="og:title" content="UANL Repo Search">
     <meta name="og:description" content="Descubre proyectos creados por alumnos y docentes de la Universidad Autónoma de Nuevo León">
     <meta name="og:type" content="website">
-    <meta name="og:image" content={logo}>
+    <meta name="og:image" content={svg}>
 
     <meta name="twitter:card" content="summary">
     <meta name="twitter:site" content="@iksaku2">
